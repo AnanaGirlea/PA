d159cbe343aa7fd2bab90ea300846099a6c66755
Anana Girlea <46604380+AnanaGirlea@users.noreply.github.com>
2023-02-16 18:24:17 +0200

    Add files via upload

---
+++ Laborator/lab03/java/task-2/src/Main.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static class Result {
+        int len; // rezultat pentru cerinta 1
+        ArrayList<Integer> subsequence; // rezultat pentru cerinta 2
+
+        public Result() {
+            len = 0;
+            subsequence = new ArrayList<>();
+        }
+    }
+
+    static class Task {
+        public final static String INPUT_FILE = "in";
+        public final static String OUTPUT_FILE = "out";
+
+        int n, m;
+        int[] v;
+        int[] w;
+
+        public void solve() {
+            readInput();
+            writeOutput(getResult());
+        }
+
+        private void readInput() {
+            try {
+                Scanner sc = new Scanner(new File(INPUT_FILE));
+                n = sc.nextInt();
+                m = sc.nextInt();
+
+                v = new int[n + 1]; // Adaugare element fictiv - indexare de la 1
+                for (int i = 1; i <= n; i++) {
+                    v[i] = sc.nextInt();
+                }
+
+                w = new int[m + 1]; // Adaugare element fictiv - indexare de la 1
+                for (int i = 1; i <= m; i++) {
+                    w[i] = sc.nextInt();
+                }
+                sc.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void writeOutput(Result result) {
+            try {
+                PrintWriter pw = new PrintWriter(new File(OUTPUT_FILE));
+                pw.printf("%d\n", result.len);
+                for (Integer x : result.subsequence) {
+                    pw.printf("%d ", x);
+                }
+                pw.printf("\n");
+                pw.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private Result getResult() {
+            Result result = new Result();
+
+            // TODO: Aflati cel mai lung subsir comun intre v (de lungime n)
+            // si w (de lungime m).
+            // Se puncteaza separat urmatoarele 2 cerinte:
+            // 2.1. Lungimea CMLSC. Rezultatul pentru cerinta aceasta se va pune
+            // in ``result.len``.
+            // 2.2. Reconstructia CMLSC. Se puncteaza orice subsir comun maximal
+            // valid. Solutia pentru aceasta cerinta se va pune in
+            // ``result.subsequence``.
+
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        new Task().solve();
+    }
+}
